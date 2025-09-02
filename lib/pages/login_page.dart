337d73f20ff25966ab3f0a4a19c0f443a5cc8e02
Solomon Njobvu <solomonnjobvu1@gmail.com>
2025-09-02 18:46:37 +0200

    Implement login page layout with email and password fields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,8 +8,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  double? _widthDevice, _heightDevice;
+
+  final GlobalKey _loginKey = GlobalKey<FormState>();
+
+  String? _emailInput;
+  String? _passwordInput;
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blueGrey[800]);
+    _widthDevice = MediaQuery.of(context).size.width;
+    _heightDevice = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: _widthDevice! * 0.05),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [_titleWidget(), _loginForm(), _submitWidgetButton()],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      "Wandar Log",
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: _heightDevice! * 0.20,
+      child: Form(
+        key: _loginKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [_textEmailField(), _passwordField()],
+        ),
+      ),
+    );
+  }
+
+  Widget _textEmailField() {
+    return TextFormField(
+      onSaved: (value) {
+        setState(() {
+          _emailInput = value;
+        });
+      },
+      validator: (value) {
+        bool results = value!.contains(
+          RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$'),
+        );
+        if (!results) {
+          return "Please enter a valid email address";
+        }
+        return null;
+      },
+      decoration: InputDecoration(hint: Text("Email")),
+    );
+  }
+
+  Widget _passwordField() {
+    return TextFormField(
+      onSaved: (value) {
+        setState(() {
+          _passwordInput = value;
+        });
+      },
+      validator: (value) => value!.length > 6
+          ? null
+          : "Please enter password greater than 6 character",
+      decoration: InputDecoration(hint: Text("Password")),
+    );
+  }
+
+  Widget _submitWidgetButton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _widthDevice! * 0.70,
+      height: _heightDevice! * 0.07,
+      color: Colors.blueGrey[900],
+      child: Text(
+        "Login",
+        style: TextStyle(fontSize: 18, color: Colors.blueGrey[100]),
+      ),
+    );
   }
 }
