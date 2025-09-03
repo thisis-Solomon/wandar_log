5e71bdf4dc93db25caad6bd385cbeae13f6246d9
Solomon Njobvu <solomonnjobvu1@gmail.com>
2025-09-03 21:01:23 +0200

    Refactor SignUpPage layout with responsive design and structured widgets

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -10,8 +10,69 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  double? _widthDevice, _heightDevice;
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blueGrey[400]);
+    _widthDevice = MediaQuery.of(context).size.width;
+    _heightDevice = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: _widthDevice! * 0.05),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _titleWidget(),
+                _registerFormWidget(),
+                _registeButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _titleWidget() {
+    return Text(
+      "Wonder Log",
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+    );
+  }
+
+  _registerFormWidget() {
+    return SizedBox(
+      height: _heightDevice! * 0.20,
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+
+  _registeButton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _widthDevice! * 0.80,
+      height: _heightDevice! * 0.07,
+      color: Colors.blueGrey[900],
+      child: Text(
+        "Register",
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: Colors.blueGrey[50],
+          letterSpacing: 1,
+        ),
+      ),
+    );
   }
 }
